03c6661916aaf95e4ab66b7ade24cdf60792e343
Amar Gupta <amar9911056196@gmail.com>
2020-05-06 01:05:19 +0530

    tempUpdate

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -2,38 +2,49 @@ const fs = require('fs');
 const http = require('http')
 const url = require('url')
 
-// // Blocking synchronus way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn)
-// const textOut = `this is what we know about ${textIn}\n on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('file written');
-
-// //Non-Blocking, asynchronus way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-//             console.log(data3);
-
-//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) =>{
-//                 console.log('your file has been written');
-//             })
-//         })
-//     })
-// });
-///////////////////////////
 // server
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , 'utf-8')
+const replaceTemplate = (temp, product) => {
+    let output =temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.  nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, '--organic')
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html` , 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html` , 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html` , 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , 'utf-8');
+
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url
+
+    // Overview Page
     if (pathName === '/' || pathName === '/over') {
-        res.end('Hello from the server')
-    } else if (pathName === '/api') {
-    res.writeHead(200, {'Content-type': 'application/json'})
-    res.end(data)
+        res.writeHead(200, {'content-type': 'text/html'})
+        const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard, el))
+        console.log(cardsHtml)
+        res.end(tempOverview)
         
+    // Product page
+    } else if (pathName === '/product') {
+        res.end('Hello from the product')
+    
+    // Api
+    } else if (pathName === '/product') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+        
+        res.end(data)
+    
+    //  Not found
     } else {
         res.writeHead(404, {
             'content-type': 'text/html',
