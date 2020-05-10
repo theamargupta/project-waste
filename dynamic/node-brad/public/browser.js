a1b4ed0d02b04fe702dd6f8cab271441060ed3d2
Amar Gupta <amar9911056196@gmail.com>
2020-05-10 14:42:56 +0530

    dom

--- dynamic/node-brad/public/browser.js
+++ dynamic/node-brad/public/browser.js
@@ -1,4 +1,22 @@
+// Create feature
+document.getElementById("create-form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+})
+
+
+
 document.addEventListener("click", (a)=>{
+    //Delete feature
+    if (a.target.classList.contains("delete-me")) {
+        if (confirm('Are you sure')){
+            axios.post('/delete-item', {id: a.target.getAttribute("data-id")}).then(()=>{
+                a.target.parentElement.parentElement.remove()
+            }).catch(()=>{
+                console.log('please tryagain...')
+            })
+        }        
+    }
+    //udate feature
     if (a.target.classList.contains("edit-me")) {
        let userInput = prompt('enter what you want to edit', a.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
        if (userInput){
--- dynamic/node-brad/server.js
+++ dynamic/node-brad/server.js
@@ -45,21 +45,21 @@ app.get('/', (req, res)=> {
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form action="/create-item" method="POST">
+          <form id="create-form" action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input id="create-feild" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
         </div>
         
-        <ul class="list-group pb-5">
+        <ul id="item-list" class="list-group pb-5">
           ${item.map((item)=>{
             return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
             <span class="item-text">${item.text}</span>
             <div>
               <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Change</button>
-              <button class="delete-me btn btn-danger btn-sm">Done</button>
+              <button data-id="${item._id}"class="delete-me btn btn-danger btn-sm">Done</button>
             </div>
           </li>`
           }).join('') }
@@ -89,4 +89,11 @@ app.post('/create-item', function(req, res) {
 app.post('/update-item', (req, res)=>{
   db.collection('item').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, ()=>{
     res.send('sucess')})
+})
+
+
+app.post('/delete-item', (req, res) =>{
+  db.collection('item').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, ()=>{
+    res.send('sucess')
+  })
 })
