eea67435e891aaa8e883fcc288973382ce8a76f2
Amar Gupta <amar9911056196@gmail.com>
2020-05-12 12:31:14 +0530

    brad

--- dynamic/node-brad/public/browser.js
+++ dynamic/node-brad/public/browser.js
@@ -1,6 +1,33 @@
+function itemTemplate(item){
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Change</button>
+      <button data-id="${item._id}"class="delete-me btn btn-danger btn-sm">Done</button>
+    </div>
+  </li>`
+}
+// intial Page load rendered
+let ourHTML = items.map((item)=>{
+    return itemTemplate(item)
+}).join('');
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
+
+
+
 // Create feature
+let createField = document.getElementById("create-field")
 document.getElementById("create-form").addEventListener("submit", (e)=>{
     e.preventDefault()
+    axios.post('/create-item', {text: createField.value}).then((response)=>{
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value=""
+        createField.focus()
+        
+    }).catch(()=>{
+        console.log('please tryagain...')
+    })
 })
 
 
--- dynamic/node-brad/server.js
+++ dynamic/node-brad/server.js
@@ -5,8 +5,6 @@ const MongoClient = mongodb.MongoClient
 let app = express()
 let db
 
-app.use(express.static('./public'))
-
 let collectionString = 'mongodb://127.0.0.1:27017'
 const database="todoapp"
 MongoClient.connect(collectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, res)=>{
@@ -14,8 +12,21 @@ MongoClient.connect(collectionString, {useNewUrlParser: true, useUnifiedTopology
   app.listen(3000)
 })
 
+app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+const passwordProtections = (req, res, next) => {
+  res.set('WWW-Authenticate', 'Basic realm="simple todo app"')
+  console.log(req.headers.authorization)
+  if (req.headers.authorization == "Basic YW1hcjoxMjM0"){
+    next()
+  } else {
+    res.status(401).send("authentication requires")
+  }
+}
+
+app.use(passwordProtections)
 
 app.get('/', (req, res)=> {
   db.collection('item').find().toArray((err, item)=>{
@@ -47,28 +58,23 @@ app.get('/', (req, res)=> {
         <div class="jumbotron p-3 shadow-sm">
           <form id="create-form" action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input id="create-feild" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
         </div>
         
         <ul id="item-list" class="list-group pb-5">
-          ${item.map((item)=>{
-            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">${item.text}</span>
-            <div>
-              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Change</button>
-              <button data-id="${item._id}"class="delete-me btn btn-danger btn-sm">Done</button>
-            </div>
-          </li>`
-          }).join('') }
+          
         </ul>
         
       </div>
       <footer>
             <p>Made with ❤️ by Amar Gupta </p>
         </footer>
+      <script>
+      let items=${JSON.stringify(item)}
+      </script>
       <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
       <script src="browser.js"></script>
     </body>
@@ -78,9 +84,9 @@ app.get('/', (req, res)=> {
   })
 
 
-app.post('/create-item', function(req, res) {
-  db.collection('item').insertOne({text: req.body.item}, ()=>{
-    res.redirect('/')
+app.post('/create-item', (req, res)=>{
+  db.collection('item').insertOne({text: req.body.text}, (err, info)=>{
+    res.json(info.ops[0])
   })
     
 })
