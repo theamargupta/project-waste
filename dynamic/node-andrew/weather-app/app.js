de0f5b5025e92c58a3dfe9ea1dffe903417f9ea0
Amar Gupta <amar9911056196@gmail.com>
2020-05-13 11:56:52 +0530

    weather

--- dynamic/node-andrew/weather-app/app.js
+++ dynamic/node-andrew/weather-app/app.js
@@ -2,5 +2,6 @@ const request = require('request');
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${process.argv[2]}&appid=4013093f340dfd0ea3c4d9eccdc3dc0e`
 request({url:url, json:true}, (err, resp) => {
 
-	console.log(resp.body)
+	const url1= resp.body
 })
+console.log(url1)
--- dynamic/node-andrew/web-server/src/app.js
+++ dynamic/node-andrew/web-server/src/app.js
@@ -1,8 +1,63 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
+//Define path for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setup handleBars engine 
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+
+//setup static drectory ot serve
+app.use(express.static (publicDirPath))
+
 app.get('', (req, res)=>{
-   res.send("hello express") 
+    res.render('index', {
+        title: 'Weather',
+        name: 'amar'
+    })
 })
 
-app.listen(3000)
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        title: 'Help',
+        name: 'amar',
+        para: 'to het the information you can do everything'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'web developer'
+    })
+})
+
+app.get('/weather', (req, res)=>{
+    res.send("show weather")
+ })
+
+ app.get('/help/*', (req, res)=>{
+     res.render('404', {
+        title: '404',
+        errMsg:'help artcile not found'
+    })
+ })
+ 
+ app.get('*', (req, res)=>{
+     res.render('404', {
+         title: '404',
+         errMsg:'page not found'
+     })
+ })
+ 
+app.listen(3000, ()=>{
+    console.log('http://127.0.0.1:3000')
+}) 
